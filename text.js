908a93267a99cd843b1172c444d96e85d1a55892
nishant <baruahnishant2003@gmail.com>
2025-02-16 22:50:15 +0530

    fix

--- text.js
+++
@@ -1,9 +0,0 @@
-          <div
-            className="
-              absolute top-full left-1/2 mt-2 pointer-events-none
-              w-60 p-3 bg-white border border-gray-200 text-gray-700 text-sm 
-              rounded-md shadow-lg transform -translate-x-1/2 
-              opacity-0 group-hover:opacity-100 transition-opacity 
-              z-50
-            "
-          >
