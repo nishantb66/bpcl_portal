551f9ffed32b83124f2698d8daa59a763d4d4f3b
nishant <baruahnishant2003@gmail.com>
2025-03-01 12:52:55 +0530

    new tcp link test

--- src/utils/kafkaClient.js
+++ src/utils/kafkaClient.js
@@ -9,7 +9,7 @@ export const getKafkaProducer = async () => {
     kafka = new Kafka({
       clientId: "my-nextjs-client",
       // Make sure this matches your KAFKA_ADVERTISED_LISTENERS
-      brokers: ["0.tcp.in.ngrok.io:19385"],
+      brokers: ["0.tcp.in.ngrok.io:18322"],
     });
   }
 
