80c078241797b7a771dd7ee6d82a9c90572eddda
nishant <baruahnishant2003@gmail.com>
2025-03-01 10:43:21 +0530

    update

--- consumer.js
+++ consumer.js
@@ -5,7 +5,7 @@ async function runConsumer() {
     // Same ngrok address
     const kafka = new Kafka({
       clientId: "my-consumer",
-      brokers: ["0.tcp.in.ngrok.io:12262"],
+      brokers: ["0.tcp.in.ngrok.io:13685"],
     });
 
     const consumer = kafka.consumer({ groupId: "login-click-group" });
