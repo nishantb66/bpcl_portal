6206a54680c03260eaa4af37dca0e0ddb8591ce2
nishant <baruahnishant2003@gmail.com>
2025-03-01 20:17:50 +0530

    tcp new link test

--- consumer.js
+++ consumer.js
@@ -5,7 +5,7 @@ async function runConsumer() {
     // Same ngrok address
     const kafka = new Kafka({
       clientId: "my-consumer",
-      brokers: ["0.tcp.in.ngrok.io:18322"],
+      brokers: ["0.tcp.in.ngrok.io:11875"],
     });
 
     const consumer = kafka.consumer({ groupId: "login-click-group" });
